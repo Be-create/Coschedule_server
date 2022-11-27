5015f09d8c2d460c2ff8cc7715a48e953a2e5be2
Benazir Sultana <benazirsultana1998@gmail.com>
2022-11-27 19:49:54 +0530

    settings changed

--- configs/db.js
+++ configs/db.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 let database = async () => {
+    let connection =`mongodb+srv://benazirsultana:${process.env.MONGO_DB_URL}G@cluster0.ahhggvd.mongodb.net/?retryWrites=true&w=majority`
     mongoose.connect(process.env.MONGO_DB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
